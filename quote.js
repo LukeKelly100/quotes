cf5158a7061ce8b86b0c7d6f4ba9a9953fed3a4f
LukeKelly100 <113995247+LukeKelly100@users.noreply.github.com>
2022-10-18 12:50:32 +0100

    Add files via upload

---
+++ quote.js
@@ -0,0 +1,17 @@
+const localQuotes = [
+    {
+    text: "I have no special talent. I am only passionately curious",
+        author: "albert einstein"
+        },
+{
+    text: "Wisely, and slow. They stumble that run fast",
+        author: "william shakespear"
+        },
+
+{
+    text: "If you judge people, you have no time to love them",
+        author: "mother teresa"
+        }
+];
+
+
