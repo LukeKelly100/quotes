4ddf0760ea7b76ca3e14466e2cd8da344e7ee4c2
LukeKelly100 <113995247+LukeKelly100@users.noreply.github.com>
2022-10-18 12:56:13 +0100

    Update quote.js

--- quote.js
+++ quote.js
@@ -1,16 +1,16 @@
-const localQuotes = [
+[
     {
-    text: "I have no special talent. I am only passionately curious",
-        author: "albert einstein"
+    "text": "I have no special talent. I am only passionately curious",
+        "author": "albert einstein"
         },
 {
-    text: "Wisely, and slow. They stumble that run fast",
-        author: "william shakespear"
+    "text": "Wisely, and slow. They stumble that run fast",
+        "author": "william shakespear"
         },
 
 {
-    text: "If you judge people, you have no time to love them",
-        author: "mother teresa"
+    "text": "If you judge people, you have no time to love them",
+        "author": "mother teresa"
         }
 ];
 
